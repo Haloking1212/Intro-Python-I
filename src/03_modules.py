7adf57af20497c1ba8cfceb0c9f0a6d786570fd8
Hector Valdez <hvaldez180@gmail.com>
2020-07-15 23:36:09 -0400

    saving

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,11 +11,19 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+print("This is the name of the script: ", sys.argv[0])
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
 
+import platform
+
+print("OS platform: ", platform.system())
+
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+
+print("python verison: ", sys.version)
 
 
 import os
@@ -24,8 +32,18 @@ import os
 # Print the current process ID
 # YOUR CODE HERE
 
+print("Current process ID: ", os.getpid())
+
 # Print the current working directory (cwd):
 # YOUR CODE HERE
 
+print("Current directory: ", os.getcwd())
+
 # Print out your machine's login name
 # YOUR CODE HERE
+
+import getpass
+
+print("Current machine's login: ", getpass.getuser())
+
+
